ea93eb69c4b241746a6cb0a7ed4328d4020eca22
malex <mircea.alexandru@gmail.com>
2016-03-07 15:03:05 +0200

    change for db schema

--- migrations/002.do.client_data.sql
+++ migrations/002.do.client_data.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS client_data
   id character varying NOT NULL,
   name character varying NOT NULL UNIQUE,
   url character varying,
-  registerType text[],
-  authType json,
+  register_type text[],
+  auth_type json,
   configured boolean,
   appkey character varying NOT NULL UNIQUE,
   CONSTRAINT pk_client_data_id PRIMARY KEY (id)
