c23cce4efc88a8b614bf1e3bb9bf641be8eb526e
malex <mircea.alexandru@gmail.com>
2016-03-18 17:11:00 +0200

    changes for user_group tbl name

--- migrations/004.do.groupsAndClients.sql
+++ migrations/004.do.groupsAndClients.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sys_user RENAME COLUMN tags TO grops;
-ALTER TABLE sys_user ADD COLUMN clients tags json[];
+ALTER TABLE sys_user ADD COLUMN clients json[];
 
-ALTER TABLE tag RENAME TO group;
+ALTER TABLE tag RENAME TO user_group;
